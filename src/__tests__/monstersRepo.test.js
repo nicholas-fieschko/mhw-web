8b0c10e16e054663a9350bab2ace62580515bf05
Nick Fieschko <nicholas.fieschko@advent.com>
2018-03-02 08:59:57 -0500

    some monster repo tests

--- src/__tests__/monstersRepo.test.js
+++ src/__tests__/monstersRepo.test.js
@@ -1,5 +1,27 @@
-import monstersRepo from 'repos/monsters';
+import { GetMonsterByName } from 'repos/monsters';
 
 describe(`Monsters data repo`, () => {
     // it('renders 1 monsterdetailview component', () => expect().toHaveLength(1));
+    describe(`GetMonsterByName`, () => {
+        describe(`a successful match`, () => {
+            let foundMonster;
+            beforeEach(() => {
+                foundMonster = GetMonsterByName('Pukei-Pukei');
+            });
+
+            it('returns the correct monster for a name search (Pukei-Pukei)', () => {
+                expect(foundMonster).toBeTruthy();
+                expect(foundMonster.name).toEqual('Pukei-Pukei');
+            });
+
+            it('returns all correct weakness data for the monster (Pukei-Pukei)', () => {
+                const weaknesses = { Fire: 2, Water: 0, Thunder: 3, Ice: 2, Dragon: 1, Poison: 1, Sleep: 3, Paralysis: 3, Blast: 2, Stun: 2 };
+                Object.keys(weaknesses).forEach(elementName => {
+                    expect(foundMonster[elementName]).toEqual(weaknesses[elementName]);
+                });
+            });
+        });
+
+        it('returns null when no match is found', () => expect(GetMonsterByName('I DONT EXIST!')).toBeNull());
+    });
 });
--- src/repos/monsters.js
+++ src/repos/monsters.js
@@ -3,26 +3,24 @@ import MonsterIcons from 'data/monster-icons';
 import anyUndefinedOrNull from 'junk-drawer/anyUndefinedOrNull';
 
 // array of monster objects with all known data {name, key, image: imagePath, Fire: 3, Water: 0, Thunder: 2, ... }
-export const MonsterData = MonsterIcons.map(({name, image, ...rest}) => ({
+export const MonsterData = MonsterIcons.map(({ name, image, ...rest }) => ({
     name,
     image,
     key: name.split(' ').join('-'),
     ...MonsterWeaknesses[name],
-    ...rest
+    ...rest,
 }));
 
-export const MonsterNameDictionary = MonsterData.reduce((monsterDict, monster) => ({
-    ...monsterDict,
-    [monster.name]: monster,
-}), {});
+export const MonsterNameDictionary = MonsterData.reduce(
+    (monsterDict, monster) => ({
+        ...monsterDict,
+        [monster.name]: monster,
+    }),
+    {}
+);
 
 export const GetMonsterByName = name => {
-    return name in MonsterNameDictionary
-        ? {
-            key: name.split(' ').join('-'),
-            ...MonsterIcons[name],
-            ...MonsterWeaknesses[name],
-        } : null;
+    return name in MonsterNameDictionary ? MonsterNameDictionary[name] : null;
 };
 
 export const sortByField = field => (monsterA, monsterB) => {
@@ -30,9 +28,9 @@ export const sortByField = field => (monsterA, monsterB) => {
     const [ A_COMES_FIRST, ARE_EQUAL, B_COMES_FIRST ] = [ -1, 0, 1 ];
     const [ monsterAProperty, monsterBProperty ] = [ monsterB[field], monsterA[field] ];
     if (anyUndefinedOrNull(monsterA, monsterB) || monsterA[field] === monsterB[field]) return ARE_EQUAL;
-    if(monsterA[field] > monsterB[field]) return A_COMES_FIRST;
-    if(monsterA[field] < monsterB[field]) return 1;
-}
+    if (monsterA[field] > monsterB[field]) return A_COMES_FIRST;
+    if (monsterA[field] < monsterB[field]) return 1;
+};
 
 export const getAllSortedMonsterData = (sortField = 'name') => {
     const sortByFunc = sortByField(sortField);
